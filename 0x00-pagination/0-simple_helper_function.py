3d968ab258474ce5b0628352ea671671a03b4126
Benjamin Torfu <btorfu@proton.me>
2024-08-19 12:27:12 +0000

    helper

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+pagination module'''
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+    '''
+    This is a simple helper function
+    Returns a tuple of the page and page size
+    '''
+

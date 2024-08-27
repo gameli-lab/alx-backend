d383039f766df03bb198d51a5ef6fa44b36ed145
Benjamin Torfu <btorfu@proton.me>
2024-08-27 14:24:56 +0000

    get

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -31,10 +31,13 @@ class Config:
 
 app.config.from_object(Config)
 
+
+@babel.localeselector
 def get_locale() -> Any:
     """
     get_locale
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 babel = Babel(app)
